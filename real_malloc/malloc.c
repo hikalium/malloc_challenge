adf6da3c81a80e96ba82be74ccf75d32a8cc30aa
hikalium <hikalium@google.com>
2021-06-11 16:41:55 +0900

    Update

--- real_malloc/malloc.c
+++ real_malloc/malloc.c
@@ -1,3 +1,59 @@
+////////////////////////////////////////////////////////////////////////////////
+/*                 (๑＞◡＜๑)  Malloc Challenge!!  (◍＞◡＜◍)                   */
+////////////////////////////////////////////////////////////////////////////////
+
+//
+// Welcome to Malloc Challenge!! Your job is to invent a smart malloc algorithm.
+//
+// Rules:
+//
+// 1. Your job is to implement my_malloc(), my_free() and my_initialize().
+//   *  my_initialize() is called only once at the beginning of each challenge.
+//      You can initialize the memory allocator.
+//   *  my_malloc(size) is called every time an object is allocated. In this
+//      challenge, |size| is guaranteed to be a multiple of 8 bytes and meets
+//      8 <= size <= 4000.
+//   * my_free(ptr) is called every time an object is freed.
+//   * Additionally, my_finalize() is called only once at the end of each challenge,
+//     so you can use this function for doing some clean ups if you want.
+// 2. The only library functions you can use in my_malloc() and my_free() are
+//    mmap_from_system() and munmap_to_system().
+//   *  mmap_from_system(size) allocates |size| bytes from the system. |size|
+//      needs to be a multiple of 4096 bytes. mmap_from_system(size) is a
+//      system call and heavy. You are expected to minimize the call of
+//      mmap_from_system(size) by reusing the returned
+//      memory region as much as possible.
+//   *  munmap_to_system(ptr, size) frees the memory region [ptr, ptr + size)
+//      to the system. |ptr| and |size| need to be a multiple of 4096 bytes.
+//      You are expected to free memory regions that are unused.
+//   *  You are NOT allowed to use any other library functions at all, including
+//      the default malloc() / free(), std:: libraries etc. This is because you
+//      are implementing malloc itself -- if you use something that may use
+//      malloc internally, it will result in an infinite recurion.
+// 3. simple_malloc(), simple_free() and simple_initialize() in simple_malloc.c
+//    are an example of straightforward implementation.
+//    Your job is to invent a smarter malloc algorithm than the simple malloc.
+// 4. There are five challenges (Challenge 1, 2, 3, 4 and 5). Each challenge
+//    allocates and frees many objects with different patterns. Your malloc
+//    is evaluated by two criteria.
+//   *  [Speed] How faster your malloc finishes the challange compared to
+//      the simple malloc.
+//   *  [Memory utilization] How much your malloc is memory efficient.
+//      This is defined as (S1 / S2), where S1 is the total size of objects
+//      allocated at the end of the challange and S2 is the total size of
+//      mmap_from_system()ed regions at the end of the challenge. You can
+//      improve the memory utilization by decreasing memory fragmentation and
+//      reclaiming unused memory regions to the system with munmap_to_system().
+// 5. This program works on Linux and Mac but not on Windows. If you don't have
+//    Linux or Mac, you can use Google Cloud Shell (See
+//    https://docs.google.com/document/d/1TNu8OfoQmiQKy9i2jPeGk1DOOzSVfbt4RoP_wcXgQSs/edit#).
+// 6. You need to specify an '-lm' option to compile this program.
+//   *  gcc malloc_challenge.c -lm
+//   *  clang malloc_challenge.c -lm
+//
+// Enjoy! :D
+//
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
